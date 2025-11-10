8a70d819605cd77cd4d9158d6fff74086bbade90
shahriarsnikdho <shahriarsanjidsnikdho@gmail.com>
2025-11-10 18:55:01 +0600

    second commit

---
+++ jun ke dekhachhi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1, num2, sum;
+
+    printf("Enter number 1:");
+    scanf("%d", &num1);
+
+    printf("Enter number 2:");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("The sum is %d\n", sum);
+    return 0;
+}
